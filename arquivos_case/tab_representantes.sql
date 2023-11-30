e0c3e5987168c171496b34624cd12d0047e301f0
victorsa2 <141345545+victorsa2@users.noreply.github.com>
2023-11-30 09:34:37 -0300

    Add files via upload

---
+++ arquivos_case/tab_representantes.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE tab_representantes 
+(
+    id_rep	INT,
+    nome_rep	VARCHAR(512),
+    email	VARCHAR(512),
+    nome_ger	VARCHAR(512),
+    nome_reg	VARCHAR(512)
+);
+
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1631989', 'Laura Barbosa', 'laurabarbosa@malwee.com', 'Fabio Souza', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1652884', 'Diego Castro', 'diegocastro@malwee.com', 'Fabio Souza', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1532754', 'Leonardo Oliveira', 'leonardooliveira@malwee.com', 'Fabio Souza', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1521624', 'Tomas Martins', 'tomasmartins@malwee.com', 'Fabio Souza', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1567556', 'Rafael Araujo', 'rafaelaraujo@malwee', 'Fabio Souza', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1652399', 'Breno Correia', 'brenocorreia@malwee.com', 'Vinicius Rocha', 'Andre Souza');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1582534', 'Eduardo Goncalves', 'eduardogoncalves@malwee.com', 'Ryan Cardoso', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1671282', 'Daniel Sousa', 'danielsousa@malwee.com', 'Ryan Cardoso', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1615246', 'Diego Azevedo', 'diegoazevedo@malwee.com', 'Emilly Sousa', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1511546', 'Nicole Cavalcanti', 'nicolecavalcanti@malwee.com', 'Erick Cardoso', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1519834', 'Bruno Cavalcanti', 'brunocavalcanti@malwee.com', 'Erick Cardoso', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1538856', 'Rafaela Ferreira', 'rafaelaferreira@malwee.com', 'Erick Cardoso', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1647626', 'Daniel Lima', 'daniellima@malwee.com', 'Danilo Rodrigues', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1633264', 'Thais Dias', 'thaisdias@malwee.com', 'Danilo Rodrigues', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1622615', 'Nicole Melo', 'nicolemelo@malwee.com', 'Danilo Rodrigues', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1480951', 'Nicole Melo', 'nicolemelo@malwee.com', 'Giovana Almeida', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1534025', 'Rafael Ferreira', 'rafaelferreira@malwee.com', 'Giovana Almeida', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1673552', 'Julia Santos', 'juliasantos@malwee.com', 'Giovana Almeida', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1677332', 'Isabelle Martins', 'isabellemartins@malwee.com', 'Giovana Almeida', 'Marina Ribeiro');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1646509', 'Guilherme Costa', 'guilhermecosta@malwee.com', 'Breno Rodrigues', 'Antonio Ferreira');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1497025', 'Ryan Gomes', 'ryangomes@malwee.com', 'Breno Rodrigues', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1507924', 'Eduardo Santos', 'eduardosantos@malwee.com', 'Thais Dias', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1516157', 'Isabela Castro', 'isabelacastro@malwee.com', 'Thais Dias', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1504076', 'Rodrigo Lima', 'rodrigolima@malwee.com', 'Paulo Sousa', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1599540', 'Lucas Souza', 'lucassouza@malwee.com', 'Paulo Sousa', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1624723', 'Giovanna Costa', 'giovannacosta@malwee.com', 'Paulo Sousa', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1497289', 'Joao Cardoso', 'joaocardoso@malwee.com', 'Samuel Martins', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1579375', 'Tiago Fernandes', 'tiagofernandes@malwee.com', 'Samuel Martins', 'Julio Azevedo');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1591520', 'Mariana Melo', 'marianamelo@malwee.com', 'Joao Cardoso', 'Sophia Fernandes');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1559743', 'Estevan Cavalcanti', 'estevancavalcanti@malwee.com', 'Joao Cardoso', 'Sophia Fernandes');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1563430', 'Amanda Pinto', 'amandapinto@malwee.com', 'Giovana Melo', 'Sophia Fernandes');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1480739', 'Rebeca Gomes', 'rebecagomes@malwee.com', 'Giovana Melo', 'Sophia Fernandes');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1606819', 'Emily Rodrigues', 'emilyrodrigues@malwee.com', 'Emily Barbosa', 'Erick Alves');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1543827', 'Kaua Carvalho', 'kauacarvalho@malwee.com', 'Emily Barbosa', 'Erick Alves');
+INSERT INTO tab_representantes (id_rep, nome_rep, email, nome_ger, nome_reg) VALUES ('1576917', 'Gabrielle Rocha', 'gabriellerocha@malwee.com', 'Alex Souza', 'Erick Alves');
